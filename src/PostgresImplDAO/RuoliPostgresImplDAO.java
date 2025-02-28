d46d66c8ee278ff4e3b7593245b72b70f33c4328
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-02-28 15:47:03 +0100

    Create RuoliPostgresImplDAO.java

---
+++ src/PostgresImplDAO/RuoliPostgresImplDAO.java
@@ -0,0 +1,55 @@
+package PostgresImplDAO;
+
+import DAO.RuoliDAO;
+import Database.DatabaseConnection;
+import Model.Ruolo;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RuoliPostgresImplDAO implements RuoliDAO {
+    private final DatabaseConnection databaseConnection;
+
+    public RuoliPostgresImplDAO() {
+        databaseConnection = DatabaseConnection.getInstance();
+    }
+
+    @Override
+    public List<Ruolo> readRuoliDi(int idCalciatore) throws SQLException {
+        ArrayList<Ruolo> ruoli = new ArrayList<>();
+        Connection connection = databaseConnection.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM ruoli WHERE id_calciatore = " + idCalciatore);
+        while (resultSet.next()) {
+            Ruolo ruolo = Ruolo.valueOf(resultSet.getString("ruolo"));
+            ruoli.add(ruolo);
+        }
+        statement.close();
+        resultSet.close();
+        return ruoli;
+    }
+
+    @Override
+    public void delete(Ruolo ruolo, int idCalciatore) throws SQLException {
+        Connection connection = databaseConnection.getConnection();
+        Statement statement = connection.createStatement();
+        statement.executeQuery(
+                "DELETE FROM ruoli WHERE ruolo = '" + ruolo.name() +
+                        "' AND id_calciatore = " + idCalciatore);
+        statement.close();
+    }
+
+    @Override
+    public void create(Ruolo ruolo, int idCalciatore) throws SQLException {
+        Connection connection = databaseConnection.getConnection();
+        Statement statement = connection.createStatement();
+        statement.executeQuery(
+                "INSERT INTO ruolo(id_calciatore, ruolo) VALUES ("
+                        + idCalciatore + ", '" + ruolo.name() + "')");
+        statement.close();
+    }
+}
