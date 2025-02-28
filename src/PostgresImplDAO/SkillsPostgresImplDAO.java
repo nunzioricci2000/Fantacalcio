269fe79d9ed2f6c4761b6e444295f4bac8e144a0
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-02-28 15:43:48 +0100

    Update SkillsPostgresImplDAO.java
    Fixed types in queries
    Removed redundant code

--- src/PostgresImplDAO/SkillsPostgresImplDAO.java
+++ src/PostgresImplDAO/SkillsPostgresImplDAO.java
@@ -12,19 +12,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SkillsPostgresImplDAO implements SkillsDAO {
-    private final DatabaseConnection databaseConnection;
+    private final Connection connection;
 
     public SkillsPostgresImplDAO() {
-        databaseConnection = DatabaseConnection.getInstance();
+        connection = DatabaseConnection.getInstance().getConnection();
     }
 
     @Override
     public List<Skill> readSkillsDi(int idCalciatore) throws SQLException {
         ArrayList<Skill> skills = new ArrayList<>();
-        Connection connection = databaseConnection.getConnection();
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(
-                "SELECT * FROM skills WHERE id_calciatore = '" + idCalciatore + "'");
+                "SELECT * FROM skills WHERE id_calciatore = " + idCalciatore);
         while (resultSet.next()) {
             Skill skill = Skill.valueOf(resultSet.getString("skill"));
             skills.add(skill);
@@ -36,21 +35,19 @@ public class SkillsPostgresImplDAO implements SkillsDAO {
 
     @Override
     public void delete(Skill skill, int idCalciatore) throws SQLException {
-        Connection connection = databaseConnection.getConnection();
         Statement statement = connection.createStatement();
         statement.executeUpdate(
                 "DELETE FROM skills WHERE skill = '" + skill.name() +
-                        "' AND id_calciatore = '" + idCalciatore + "'");
+                        "' AND id_calciatore = " + idCalciatore);
         statement.close();
     }
 
     @Override
     public void create(Skill skill, int idCalciatore) throws SQLException {
-        Connection connection = databaseConnection.getConnection();
         Statement statement = connection.createStatement();
         statement.executeUpdate(
                 "INSERT INTO skills(id_calciatore, skill) VALUES ("
-                        + idCalciatore + ", " + skill.name() + ")");
+                        + idCalciatore + ", '" + skill.name() + "')");
         statement.close();
     }
 }
