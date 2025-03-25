97d31219f5a7a80e9e8f813f9c8e84aa9ce6cef4
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-03-25 17:16:23 +0100

    Update MilitanzaDAO.java

--- src/DAO/MilitanzaDAO.java
+++ src/DAO/MilitanzaDAO.java
@@ -2,12 +2,13 @@ package DAO;
 
 import Model.Calciatore;
 import Model.Militanza;
+import Model.Squadra;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface MilitanzaDAO {
-    List<Militanza> getCarrieraDi(Calciatore calciatore);
-    Militanza update(Militanza militanza);
-    void delete(Militanza militanza);
-    Militanza create(Militanza militanza);
+    List<Militanza> getCarrieraDi(int idCalciatore) throws SQLException;
+    void delete(int idCalciatore, Squadra squadra) throws SQLException;
+    Militanza create(int idCalciatore, Militanza militanza) throws SQLException;
 }
